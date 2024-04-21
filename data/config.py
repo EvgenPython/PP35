35ecbd4ef37a5f36d1794b5160fd97e6c2aaf842
Djekson <alekssyvorov29@gmail.com>
2024-04-21 18:04:13 +0300

    444444

--- data/config.py
+++ data/config.py
@@ -6,5 +6,7 @@ env.read_env()
 
 BOT_TOKEN = env.str("BOT_TOKEN")  # Забираем значение типа str
 ADMINS = env.list("ADMINS")  # Тут у нас будет список из админов
+CLIENT_ID = env.str('api_ad_client')
+SECRET_KEY = env.str("secret_key")
 IP = env.str("ip")  # Тоже str, но для айпи адреса хоста
 
--- handlers/users/__init__.py
+++ handlers/users/__init__.py
@@ -1,3 +1,5 @@
+from . import data_udemy
+from . import date_categories
 from . import help
 from . import start
 from . import echo
